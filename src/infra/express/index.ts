a993beb33d2143e52b742eb3b101139f7dc41219
Iago Abner <iagoabner.dev@gmail.com>
2023-02-23 23:49:24 -0300

    feat: create route to list all users

--- src/infra/express/index.ts
+++ src/infra/express/index.ts
@@ -1,11 +1,20 @@
+import cors from 'cors'
 import express, { Request, Response } from 'express'
 import { CreateUserUseCase } from '../../application/create-user.use-case'
+import { ListUsersUseCase } from '../../application/list-users.use-case'
 import { UserInMemory } from '../database/user-in-memory.repository'
 
 const app = express()
 const repo = new UserInMemory()
 
+app.use(cors())
 app.use(express.json())
+
+app.get('/', async (_, res) => {
+  const listAllUsers = new ListUsersUseCase(repo)
+  const result = await listAllUsers.execute()
+  return res.status(200).send(result)
+})
 
 app.post('/', async (req: Request, res: Response) => {
   const createUserUseCase = new CreateUserUseCase(repo)
